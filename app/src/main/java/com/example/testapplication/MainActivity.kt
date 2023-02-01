6ce02ce67e16c2895a2089afde3d4d21e19afd3d
qqw2 <whathappensifistart@gmail.com>
2023-02-01 14:10:41 +0200

    First Commit

---
+++ app/src/main/java/com/example/testapplication/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.testapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+    //asdf asdf asdf
+    //asdfsdf
+}
